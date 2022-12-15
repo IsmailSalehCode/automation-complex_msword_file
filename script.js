e618340eb319a46b89e12d80dfb5ec585661514a
ISMAIL SALEH <centriu78@gmail.com>
2022-12-15 03:14:45 +0200

    prompt sync

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 import * as fs from "fs";
 import pkg from "docx";
 const { Document, Packer, Paragraph, TextRun } = pkg;
+const prompt = require("prompt-sync")();
 
-// Documents contain sections, you can have multiple sections per document, go here to learn more about sections
-// This simple example will only contain one section
-const doc = new Document({
-  sections: [
-    {
-      properties: {},
-      children: [
-        new Paragraph({
-          children: [
-            new TextRun("Hello World"),
-            new TextRun({
-              text: "Foo Bar",
-              bold: true,
-            }),
-            new TextRun({
-              text: "\tGithub is the best",
-              bold: true,
-            }),
-          ],
-        }),
-      ],
-    },
-  ],
-});
+const fname = prompt("Enter file name: ");
+generateDoc(fname);
 
-// Used to export the file into a .docx file
-Packer.toBuffer(doc).then((buffer) => {
-  fs.writeFileSync("My Document.docx", buffer);
-});
+function generateDoc(fname) {
+  // Documents contain sections, you can have multiple sections per document, go here to learn more about sections
+  // This simple example will only contain one section
+  const doc = new Document({
+    sections: [
+      {
+        properties: {},
+        children: [
+          new Paragraph({
+            children: [
+              new TextRun("Hello World"),
+              new TextRun({
+                text: "Foo Bar",
+                bold: true,
+              }),
+              new TextRun({
+                text: "\tGithub is the best",
+                bold: true,
+              }),
+            ],
+          }),
+        ],
+      },
+    ],
+  });
 
-// Done! A file called 'My Document.docx' will be in your file system.
+  // Used to export the file into a .docx file
+  Packer.toBuffer(doc).then((buffer) => {
+    fs.writeFileSync(fname + ".docx", buffer);
+  });
+
+  // Done! A file called 'fname' will be in your file system.
+}
